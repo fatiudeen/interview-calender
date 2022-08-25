2adf59f2e398ca2d99a2ffee3de92bed8cd97df5
fatiudeen <shehufatiudeen@gmail.com>
2022-08-25 14:37:52 +0100

    jest config

---
+++ src/api/v1/__test__/index.test.ts
@@ -0,0 +1,12 @@
+import supertest from 'supertest';
+import app from '@app';
+
+describe('index test', () => {
+  it('should return 200', async () => {
+    const { statusCode, body } = await supertest(app.instance()).get('/');
+
+    expect(statusCode).toBe(200);
+    expect(body.success).toEqual(true);
+    expect(body.message).toEqual('WELCOME TO THE INTERVIEW CALENDAR');
+  });
+});
