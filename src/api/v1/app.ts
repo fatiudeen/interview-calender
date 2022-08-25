5d650058bf6713274378d4c83f9e4edbffb99c44
fatiudeen <shehufatiudeen@gmail.com>
2022-08-25 05:46:33 +0100

    configured  express app

---
+++ src/api/v1/app.ts
@@ -0,0 +1,52 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable lines-between-class-members */
+import express, { Application } from 'express';
+import cors from 'cors';
+import logger from 'morgan';
+import interviewRoute from '@routes/interview.route';
+import userRoute from '@routes/user.route';
+import error404 from '@middlewares/404handler';
+import { errorHandler } from '@middlewares/errorHandler';
+import docs from '@middlewares/docs';
+
+class App {
+  private app: Application;
+  constructor() {
+    this.app = express();
+    this.initMiddlewares();
+    this.initRoutes();
+    this.initErrorHandlers();
+  }
+
+  private initRoutes() {
+    this.app.use('/', (req, res) => {
+      res.status(200).json({ message: 'WELCOME TO THE INTERVIEW CALENDAR' });
+    });
+    this.app.use('/interview', interviewRoute.setupRoutes());
+    this.app.use('/users', userRoute.setupRoutes());
+    this.app.use('/docs', docs);
+  }
+  private initMiddlewares() {
+    this.app.use(
+      cors({
+        origin: ['*'],
+      }),
+    );
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(logger('dev'));
+    this.app.use(express.json());
+  }
+
+  private initErrorHandlers() {
+    this.app.use(errorHandler);
+    this.app.use('*', error404);
+  }
+
+  public listen(port: number) {
+    this.app.listen(port, () => {
+      console.log(`running on port ${port}`);
+    });
+  }
+}
+
+export default new App();
